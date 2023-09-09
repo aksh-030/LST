a77db2f4c7bf7a2f03c604bbe12c14dbdf00f7e5
Akshitha <74759004+aksh-030@users.noreply.github.com>
2023-09-09 08:44:50 +0530

    Update trans_file.py

--- scripts/trans_file.py
+++ scripts/trans_file.py
@@ -131,37 +131,3 @@ def exec_file(path):
         data = json.loads(r.text)
         translated += data["translatedText"]
     return translated
-
-
-
-
-
-
-
-
-
-'''# Imports
-import fitz
-import requests
-import json
-
-# Function to split large string
-def splitter(n, s):
-    pieces = s.split()
-    return (" ".join(pieces[i:i+n]) for i in range(0, len(pieces), n))
-
-def exec_file(path):
-    # Open and extract text
-    doc = fitz.open(path)
-    text = ""
-    for page in doc:
-        text+=page.get_text()
-    #print(text)
-
-    # Translated
-    translated=""
-    for piece in splitter(500, text):
-        r = requests.post('http://127.0.0.1:5000/translate', data={'q': piece,'source': "auto",'target': "en",'format': "text",'api_key': ""})
-        data = json.loads(r.text)
-        translated+=data["translatedText"]
-    return translated'''
